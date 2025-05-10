6fbb641d6bb08318f08192105ea8d39b112c2d37
ruannysil <ruannysilva67@gmail.com>
2025-05-09 21:55:26 -0300

    feat: add client map

--- app/components/map.tsx
+++ app/components/map.tsx
@@ -1,3 +1,5 @@
+'use client'
+
 import React from 'react'
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
 
@@ -6,41 +8,41 @@ export interface MapsProps {
   center: { latitude: number; longitude: number },
 }
 
-export default function Map({center, people}: MapsProps) {
+export default function Map({ center, people }: MapsProps) {
   return (
     <div className='mt-6'>
-          <MapContainer
-            center={[center.latitude, center.longitude]}
-            zoom={5}
-            scrollWheelZoom={true}
-            className="h-[500px] rounded-xl shadow-md"
-          >
-            <TileLayer
-              attribution="&copy; OpenStreetMap contributors"
-              url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-            />
-            {people
-              .filter(person =>
-                typeof person.latitude === 'number' &&
-                typeof person.longitude === 'number' &&
-                !isNaN(person.latitude) &&
-                !isNaN(person.longitude)
-              )
-              .map((person, index) => (
-                <Marker key={index} position={[person.latitude, person.longitude]}>
-                  <Popup>
-                    <div>
-                      {/* <strong>{person.name}</strong> */}
-                      <br />
-                      Status: {person.status}
-                      <br />
-                      Último acesso: <br />
-                      {new Date(person.ultimaRequisicao).toLocaleString()}
-                    </div>
-                  </Popup>
-                </Marker>
-              ))}
-          </MapContainer>
-        </div>
+      <MapContainer
+        center={[center.latitude, center.longitude]}
+        zoom={5}
+        scrollWheelZoom={true}
+        className="h-[500px] rounded-xl shadow-md"
+      >
+        <TileLayer
+          attribution="&copy; OpenStreetMap contributors"
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        {people
+          .filter(person =>
+            typeof person.latitude === 'number' &&
+            typeof person.longitude === 'number' &&
+            !isNaN(person.latitude) &&
+            !isNaN(person.longitude)
+          )
+          .map((person, index) => (
+            <Marker key={index} position={[person.latitude, person.longitude]}>
+              <Popup>
+                <div>
+                  {/* <strong>{person.name}</strong> */}
+                  <br />
+                  Status: {person.status}
+                  <br />
+                  Último acesso: <br />
+                  {new Date(person.ultimaRequisicao).toLocaleString()}
+                </div>
+              </Popup>
+            </Marker>
+          ))}
+      </MapContainer>
+    </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 'use client';
 
-import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
 import L from 'leaflet';
 import 'leaflet/dist/leaflet.css';
 import { useEffect, useMemo, useState } from 'react';
